c1ca15af5879f864e624ef94eb8fd6d9186a1a27
Bharat Kumar Jain <33486187+BharatKJain@users.noreply.github.com>
2021-04-03 14:25:25 +0530

    Updated: code structure for deployment

--- bin/build-html.js
+++
@@ -1,8 +0,0 @@
-const pug=require("pug");
-const fs=require("fs");
-var fsWriteStream=fs.createWriteStream("../dev-files/index.html")
-
-const compiledFunction = pug.compileFile("./../views/index.pug");
-
-console.log(compiledFunction());
-
--- index.js
+++ index.js
@@ -4,7 +4,7 @@
  * Module dependencies.
  */
 
-var app = require('../app');
+var app = require('./app');
 var debug = require('debug')('sample-app:server');
 var http = require('http');
 
